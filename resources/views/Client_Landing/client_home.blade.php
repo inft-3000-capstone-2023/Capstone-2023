640557dd2e6944714b709c4dca19961c90be033b
Yitian Yang <w0454055@nscc.ca>
2023-02-28 01:26:59 -0400

    Ongoing

--- resources/views/Client_Landing/client_home.blade.php
+++ resources/views/Client_Landing/client_home.blade.php
@@ -4,17 +4,31 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <div>
+                <link rel="stylesheet" href="<?php echo asset('css/client_home.css')?>" type="text/css">
+                <div class="home-container">
+                    <div class="home-hero">
+                        <div class="home-container1">
+                            <h1 class="home-text">Create your event today..</h1>
+                            <span class="home-text1">
+          <span>
+            <span>
+              Welcome to NETS, your one-stop solution for creating your own event listing store. Our platform provides a wide range of services for clients who want to promote and sell tickets for their events online.
+            </span>
+            <span></span>
+          </span>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        </span>
+                            <div class="home-btn-group">
+                                <button class="home-button button">Create your event</button>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <img
+                            alt="image"
+                            src="https://images.unsplash.com/photo-1517263904808-5dc91e3e7044?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
+                            class="home-image"
+                        />
+                    </div>
                 </div>
             </div>
         </div>
