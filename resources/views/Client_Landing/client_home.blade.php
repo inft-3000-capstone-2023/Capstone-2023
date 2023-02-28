65561daedbea55cc5d8e2bd00ddb2330bd62a239
Yitian Yang <w0454055@nscc.ca>
2023-02-28 15:51:24 -0400

    finish client landing page

--- resources/views/Client_Landing/client_home.blade.php
+++ resources/views/Client_Landing/client_home.blade.php
@@ -1,37 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div>
-                <link rel="stylesheet" href="<?php echo asset('css/client_home.css')?>" type="text/css">
-                <div class="home-container">
-                    <div class="home-hero">
-                        <div class="home-container1">
-                            <h1 class="home-text">Create your event today..</h1>
-                            <span class="home-text1">
-          <span>
-            <span>
-              Welcome to NETS, your one-stop solution for creating your own event listing store. Our platform provides a wide range of services for clients who want to promote and sell tickets for their events online.
-            </span>
-            <span></span>
-          </span>
+    <div class="container">
+        <div class="row justify-content-center">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <div class="col-md-8">
+                <!--This is the font section-->
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&amp;display=swap"
+                    data-tag="font"
+                />
+                <!--This is the global style section-->
+                <link rel="stylesheet" href="css/style.css" />
 
-        </span>
-                            <div class="home-btn-group">
-                                <button class="home-button button">Create your event</button>
-                            </div>
+                <div>
+                    <!--This is the client homepage style section-->
+                    <link href="css/client_home.css" rel="stylesheet" />
+
+                    <div class="home-container">
+                        <div class="home-hero">
+                            <span class="home-text">WELCOME TO NETS</span>
+                            <h1 class="home-text01">Create your event today..</h1>
+                            <button class="home-button button">Get Started</button>
                         </div>
-                        <img
-                            alt="image"
-                            src="https://images.unsplash.com/photo-1517263904808-5dc91e3e7044?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80"
-                            class="home-image"
-                        />
+                        <div class="home-banner">
+                            <h1 class="home-text02">Why NETS?</h1>
+                            <span class="home-text03">
+            <span>
+              With our user-friendly interface and customizable templates, you
+              can easily create an event listing store that matches your brand
+              and style. You can showcase all the important details about your
+              events, such as date, time, location, and ticket pricing.
+            </span>
+            <br />
+            <br />
+            <span>
+              Our platform also provides advanced features such as ticketing and
+              registration, which allows you to manage your attendees and track
+              your event's success. You can even set up discounts and promotions
+              to incentivize ticket sales and generate more revenue.
+            </span>
+            <br />
+            <br />
+            <span>
+              But that's not all. We also offer marketing and promotion services
+              to help you reach a wider audience and increase your event's
+              visibility. Our team of experts can help you with social media
+              marketing, email marketing, and other digital marketing strategies
+              to make sure your events get the attention they deserve.
+            </span>
+            <br />
+            <br />
+            <span>
+              With NETS, you can create an event listing store that stands out
+              from the crowd and delivers a seamless experience for your
+              attendees. So why wait? Sign up today and start creating your own
+              event listing store!
+            </span>
+            <br />
+          </span>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
         </div>
     </div>
-</div>
 @endsection
