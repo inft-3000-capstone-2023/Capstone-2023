c95931107210e6906716f5bd5d56f882513c5370
Yitian <w0454055@nscc.ca>
2023-03-26 17:02:40 -0300

    Events could display on client/id/events page + event creation complete, and routes fixed for EventController. Changing eventcontroller routes in a nicer way and add alert status on client-events-index page.

--- routes/web.php
+++ routes/web.php
@@ -34,24 +34,18 @@ Route::resource('admin', App\Http\Controllers\ClientController::class)->middlewa
 // TODO Will need to add middleware here to prevent unauthorized access
 
 //routes for client events index and creation page
-Route::get('/client/{client}/events', [App\Http\Controllers\EventController::class,
-    'client_events'])->name('client_events');
-Route::get('/client/{client}/events/createS1', [App\Http\Controllers\EventController::class,
-    'createS1'])->name('createS1');
-Route::get('/client/{client}/events/createS2', [App\Http\Controllers\EventController::class,
-    'createS2'])->name('createS2');
-Route::get('/client/{client}/events/createS3', [App\Http\Controllers\EventController::class,
-    'createS3'])->name('createS3');
-Route::get('/client/{client}/events/createS4', [App\Http\Controllers\EventController::class,
-    'createS4'])->name('createS4');
-Route::post('/client/{client}/events/createS1', [App\Http\Controllers\EventController::class,
-    'postcreateS1'])->name('postcreateS1');
-Route::post('/client/{client}/events/createS2', [App\Http\Controllers\EventController::class,
-    'postcreateS2'])->name('postcreateS2');
-Route::post('/client/{client}/events/createS3', [App\Http\Controllers\EventController::class,
-    'postcreateS3'])->name('postcreateS3');
-Route::put('/client/{client}/events/createS4', [App\Http\Controllers\EventController::class,
-    'postcreateS4'])->name('postcreateS4');
+Route::prefix('/client/{client}/events')->group(function () {
+    Route::get('/', [App\Http\Controllers\EventController::class, 'client_events'])->name('client_events');
+    Route::get('/createS1', [App\Http\Controllers\EventController::class, 'createS1'])->name('createS1');
+    Route::get('/createS2', [App\Http\Controllers\EventController::class, 'createS2'])->name('createS2');
+    Route::get('/createS3', [App\Http\Controllers\EventController::class, 'createS3'])->name('createS3');
+    Route::get('/createS4', [App\Http\Controllers\EventController::class, 'createS4'])->name('createS4');
+    Route::post('/createS1', [App\Http\Controllers\EventController::class, 'postcreateS1'])->name('postcreateS1');
+    Route::post('/createS2', [App\Http\Controllers\EventController::class, 'postcreateS2'])->name('postcreateS2');
+    Route::post('/createS3', [App\Http\Controllers\EventController::class, 'postcreateS3'])->name('postcreateS3');
+    Route::put('/createS4', [App\Http\Controllers\EventController::class, 'postcreateS4'])->name('postcreateS4');
+});
+
 
 
 
