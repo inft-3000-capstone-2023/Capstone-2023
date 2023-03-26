4b35b12a42be43c22e6bf74d4887160dd5ad2b08
Yitian <w0454055@nscc.ca>
2023-03-26 01:54:48 -0300

    Events could display on client/id/events page + event creation complete, and routes fixed for EventController.

--- database/migrations/2023_02_08_013157_create_events_table.php
+++ database/migrations/2023_02_08_013157_create_events_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->increments(id());
+            $table->id();
             $table->timestamps();
             $table->foreignId('client_id');
             $table->string('event_title');
